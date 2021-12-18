ffabf08b6b2b2ee532d3b606faefafdb2a19ab24
unknown <shaharvind340@gmail.com>
2021-12-18 14:11:39 +0530

    Functions

---
+++ tut53.js
@@ -0,0 +1,25 @@
+console.log("This is tuttorial 53");
+
+function greet(name, number = "Number from Javascript.") {
+    console.log(name + "is a Random");
+    console.log(number + " number in " + name);
+}
+
+
+function sum(a, b) {
+    let c = a + b;
+    // console.log(c);
+    return c;
+}
+
+// let name1 = "Harry ";
+// let name2 = "rrry";
+// let name3 = "rhgrrry";
+// let name4 = "rgdn";
+// let random = "Random ";
+// greet(random, 6); 
+// greet(name1, 5);
+// greet(name2)
+
+let d = sum(2, 3);
+console.log(d);
